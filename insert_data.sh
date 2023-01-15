7a7a0020ee4a31cc299855560dfdfa0bd682c320
gabester0 <gceipper@gmail.com>
2023-01-15 18:28:16 -0500

    Added from virtual workspace

---
+++ insert_data.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+cat ./games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != 'year' ]]
+    then
+      WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER';")"
+      if [[ -z $WINNER_ID ]]
+      then
+        INSERT_WINNER_RESULT="$($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")"
+        echo $INSERT_WINNER_RESULT
+      fi
+
+      OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT';")"
+      if [[ -z $OPPONENT_ID ]]
+      then
+        INSERT_OPPONENT_RESULT="$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');")"
+        echo $INSERT_OPPONENT_RESULT
+      fi
+
+      # Check for existing WINNER_ID
+      if [[ -z $WINNER_ID ]]
+      then
+      # Get WINNER_ID if missing
+        WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER';")"
+      fi
+      # Check for existing OPPONENT_ID
+      if [[ -z $OPPONENT_ID ]]
+      then
+        # Get OPPONENT_ID if missing
+        OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT';")"
+      fi
+
+      # echo $WINNER_ID $OPPONENT_ID
+      INSERT_GAME_RESULT="$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES('$YEAR', '$ROUND', '$WINNER_ID', '$OPPONENT_ID', '$WINNER_GOALS', '$OPPONENT_GOALS');")"
+      echo $INSERT_GAME_RESULT
+
+    fi
+done
